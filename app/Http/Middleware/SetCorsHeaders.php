cd7444fde6c2bfa1fa78947e6b33c64403af8934
memochou1993 <memochou1993@gmail.com>
2019-04-07 22:26:32 +0800

    Modify Kernel and SetCorsHeaders middleware

--- app/Http/Middleware/SetCorsHeaders.php
+++ app/Http/Middleware/SetCorsHeaders.php
@@ -15,7 +15,8 @@ class SetCorsHeaders
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*');
+        return $next($request) 
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', '*'); 
     }
 }
