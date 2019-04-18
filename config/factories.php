db3a5ed6175ff760b6b02d37acb252f797c9129d
memochou1993 <memochou1993@gmail.com>
2019-04-19 00:16:11 +0800

    Modify UserFactory

--- config/factories.php
+++ config/factories.php
@@ -7,6 +7,13 @@ return [
         'email' => 'admin@email.com',
         'email_verified_at' => now(),
         'password' => '$argon2i$v=19$m=1024,t=2,p=2$UUxxaWZ1akM1dzlmbEtiNA$+Q2+IURHJIe0xYmLds0j3x59SeC+77W6/Qp9wR85jQY', // secret
+        'settings' => json_encode([
+            'key' => [
+                'strict' => true,
+                'paginate' => 15,
+                'pagination' => false,
+            ],
+        ]),
     ],
 
 ];
