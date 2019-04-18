2c2b9636d1c628c48b4e67fb74e513682d3d50d4
memochou1993 <memochou1993@gmail.com>
2019-04-19 00:15:22 +0800

    Modify SetCorsHeaders middleware

--- app/Http/Middleware/SetCorsHeaders.php
+++ app/Http/Middleware/SetCorsHeaders.php
@@ -15,9 +15,12 @@ class SetCorsHeaders
      */
     public function handle($request, Closure $next)
     {
-        return $next($request) 
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Headers', 'Content-Type');
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', '*');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
+
+        return $response;
     }
 }
