1907f0a0aed34bec91be3805925bcf4ccef5f8ba
memochou1993 <memochou1993@gmail.com>
2019-04-23 16:33:50 +0800

    Modify SetCorsHeaders middleware

--- app/Http/Middleware/SetCorsHeaders.php
+++ app/Http/Middleware/SetCorsHeaders.php
@@ -18,7 +18,7 @@ class SetCorsHeaders
         $response = $next($request);
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
