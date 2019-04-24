0bb44cf5e823a19d0344aef8b04f15cf7128ee97
memochou1993 <memochou1993@gmail.com>
2019-04-24 18:05:11 +0800

    Remove settings field

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,12 +22,5 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$argon2i$v=19$m=1024,t=2,p=2$UUxxaWZ1akM1dzlmbEtiNA$+Q2+IURHJIe0xYmLds0j3x59SeC+77W6/Qp9wR85jQY', // secret
         'remember_token' => Str::random(10),
-        'settings' => json_encode([
-            'key' => [
-                'strict' => true,
-                'paginate' => 15,
-                'pagination' => false,
-            ],
-        ]),
     ];
 });
